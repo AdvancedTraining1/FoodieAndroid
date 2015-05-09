c728c48cfee1d65023987325ee33a1eacfa7128c
MengJaeSuk <MengJaeSuk@gmail.com>
2015-05-09 11:38:22 +0800

    .gitignore

--- src/com/bjtu/foodie/UI/AddMomentActivity.java
+++ src/com/bjtu/foodie/UI/AddMomentActivity.java
@@ -1,12 +1,25 @@
 package com.bjtu.foodie.UI;
 
-import com.bjtu.foodie.R;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.bjtu.foodie.R;
+import com.bjtu.foodie.model.Moment;
+import com.bjtu.foodie.utils.MomentTalkToServer;
 
 public class AddMomentActivity extends Activity {
 	
@@ -29,11 +42,35 @@ public class AddMomentActivity extends Activity {
 		switch (item_id) {
 		case R.id.new_action_send:
 			Toast.makeText(this, "Sending~~", Toast.LENGTH_SHORT).show();
+			Moment moment = new Moment("just test 1");
+			AddMomentTask task = new AddMomentTask();
+			task.moment = moment;
+			task.execute();
 			break;
-
 		default:
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void chooseImage(View view){
+		Toast.makeText(getApplicationContext(), "choose image",
+			     Toast.LENGTH_SHORT).show();
+	}
+	
+	
+}
+
+class AddMomentTask extends AsyncTask<Object, Object, Object>{
+	Moment moment;
+
+	@Override
+	protected Object doInBackground(Object... arg0) {
+		List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+		postParameters.add(new BasicNameValuePair("token", "554d7c41824fb683430ff4e1"));
+        postParameters.add(new BasicNameValuePair("content", moment.getContent()));
+        System.out.println("----------------------"+moment.getContent());
+        MomentTalkToServer.momentPost("moment/addMoment", postParameters);
+		return null;
 	}
 }
