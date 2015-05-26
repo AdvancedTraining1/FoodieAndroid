3cd831cbaa77fdd714f7b01fec781aaac1e82d40
ytwang09 <ytwang09@gmail.com>
2015-05-26 16:26:51 +0800

    add listview adapter in mainpage and change icon_foodie

--- src/com/bjtu/foodie/UI/MainActivity.java
+++ src/com/bjtu/foodie/UI/MainActivity.java
@@ -1,10 +1,11 @@
 package com.bjtu.foodie.UI;
 
-
 import com.bjtu.foodie.R;
+import com.bjtu.foodie.adapter.RestaurantRecommendItemAdapter;
+import com.bjtu.foodie.data.TestData;
+
 import android.view.View.OnClickListener;
 import cn.jpush.android.api.JPushInterface;
-
 
 import android.app.ActionBar;
 import android.app.Activity;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.Window;
 
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	private Button btn_moment;
@@ -25,93 +27,102 @@ public class MainActivity extends Activity {
 	private Button btn_login;
 	private Button btn_friend;
 
+	private ListView lv_restRecommend;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mainpage);
+
+		lv_restRecommend = (ListView) findViewById(R.id.lv_restRecommend);
 		
-		
-		
-		m_ToDish = (Button)findViewById(R.id.btn_dish);
-		//btn_register = (Button)findViewById(R.id.btn_register);
-		
+		/**
+		 * only use test data temporarily
+		 */
+		lv_restRecommend.setAdapter(new RestaurantRecommendItemAdapter(
+				getApplicationContext(), TestData.getRecommendsData()));
+		m_ToDish = (Button) findViewById(R.id.btn_dish);
+		// btn_register = (Button)findViewById(R.id.btn_register);
+
 		JPushInterface.setDebugMode(true);
-        JPushInterface.init(this);
+		JPushInterface.init(this);
 
-        btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new OnClickListener() {
-			
+		btn_login = (Button) findViewById(R.id.btn_login);
+		btn_login.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						LoginActivity.class);
 				startActivity(i);
 			}
 
 		});
-        
-        btn_register = (Button) findViewById(R.id.btn_register);
-        btn_register.setOnClickListener(new OnClickListener() {
-			
+
+		btn_register = (Button) findViewById(R.id.btn_register);
+		btn_register.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						RegisterActivity.class);
 				startActivity(i);
 			}
 
 		});
-        
-        
-        m_ToDish.setOnClickListener(new OnClickListener() {
-            @Override
-			public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, DishMenu.class);
-                startActivityForResult(intent, 0);
-            }
-        });
 
-        /*btn_register.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Register",Toast.LENGTH_LONG ).show();
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, RegisterActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });*/
-        btn_moment = (Button) findViewById(R.id.btn_moment);
-        btn_moment.setOnClickListener(new OnClickListener() {
-			
+		m_ToDish.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), MomentsActivity.class);
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, DishMenu.class);
+				startActivityForResult(intent, 0);
+			}
+		});
+
+		/*
+		 * btn_register.setOnClickListener(new OnClickListener() { public void
+		 * onClick(View v) { Toast.makeText(MainActivity.this,
+		 * "Register",Toast.LENGTH_LONG ).show(); Intent intent = new Intent();
+		 * intent.setClass(MainActivity.this, RegisterActivity.class);
+		 * startActivityForResult(intent, 0); } });
+		 */
+		btn_moment = (Button) findViewById(R.id.btn_moment);
+		btn_moment.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(),
+						MomentsActivity.class);
 				startActivity(i);
 			}
 		});
-        
-        btn_date = (Button) findViewById(R.id.btn_date);
-        btn_date.setOnClickListener(new OnClickListener() {
-			
+
+		btn_date = (Button) findViewById(R.id.btn_date);
+		btn_date.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), DatesActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						DatesActivity.class);
 				startActivity(i);
 			}
 
 		});
-        
-        btn_friend = (Button) this.findViewById(R.id.btn_friend);
-        btn_friend.setOnClickListener(new OnClickListener() {
-			
+
+		btn_friend = (Button) this.findViewById(R.id.btn_friend);
+		btn_friend.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), FriendListActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						FriendListActivity.class);
 				startActivity(i);
 			}
 
 		});
 	}
-
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -124,16 +135,15 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@Override
-    public void onResume() {
-        super.onResume();
-        JPushInterface.onResume(this);
-    }
-	
+	public void onResume() {
+		super.onResume();
+		JPushInterface.onResume(this);
+	}
+
 	@Override
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
 		JPushInterface.onPause(this);
 	}
--- src/com/bjtu/foodie/data/TestData.java
+++ src/com/bjtu/foodie/data/TestData.java
@@ -7,13 +7,15 @@ import java.util.List;
 import com.bjtu.foodie.R;
 import com.bjtu.foodie.model.DateModel;
 import com.bjtu.foodie.model.Moment;
+import com.bjtu.foodie.model.Restaurant;
 import com.bjtu.foodie.model.User;
 
 public class TestData {
 	private List<Moment> moments;
 	private List<DateModel> dates;
 	private List<User> friends;
-	
+	private static List<Restaurant> restaurants;
+
 	public List<Moment> getMomentsData() {
 		moments = new ArrayList<Moment>();
 		for (int i = 0; i < 10; ++i) {
@@ -25,26 +27,36 @@ public class TestData {
 		}
 		return moments;
 	}
-	
+
 	public List<DateModel> getDatesData() {
 		dates = new ArrayList<DateModel>();
 		for (int i = 0; i < 10; ++i) {
 			DateModel d = new DateModel(String.format("%d", i), String.format(
-					"date%d", i), R.drawable.icon_avatar,"Barbecue",
+					"date%d", i), R.drawable.icon_avatar, "Barbecue",
 					"Address: barbecue in huoshaoshi", new Date(
 							System.currentTimeMillis()), new Date(
-							System.currentTimeMillis()),3);
+							System.currentTimeMillis()), 3);
 			dates.add(d);
 		}
 		return dates;
 	}
-	
+
 	public List<User> getFriendsData() {
 		friends = new ArrayList<User>();
 		for (int i = 0; i < 10; ++i) {
-			User f = new User(String.format("friend%d", i),"123","123");
+			User f = new User(String.format("friend%d", i), "123", "123");
 			friends.add(f);
 		}
 		return friends;
+	}
+
+	public static List<Restaurant> getRecommendsData() {
+		restaurants = new ArrayList<Restaurant>();
+		for (int i = 0; i < 7; ++i) {
+			Restaurant r = new Restaurant("123", "abc" + i, i, null, null,
+					null, null, "fast food-No."+i, 0, 0, 0, 0, 0, 0);
+			restaurants.add(r);
+		}
+		return restaurants;
 	}
 }
